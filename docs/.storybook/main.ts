314bb3832ebaf13f8d4c5da4fbb004f030bc537a
Michael Suzuki <legal@kigumi.style>
2026-03-13 19:00:36 +0100

    docs: hide VueGuide in storybook

--- docs/.storybook/main.ts
+++ docs/.storybook/main.ts
@@ -1,11 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: [
-    '../src/**/*.mdx',
-    '!../src/stories/VueGuide.mdx',
-    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   core: {
     disableTelemetry: true,
   },
